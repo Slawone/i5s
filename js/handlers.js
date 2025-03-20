cda949173a8f530ab9bd5783c743f2f991f56abe
Slawone <for1s8@ya.ru>
2025-03-20 09:30:49 +0300

    add scroll sections

--- js/handlers.js
+++ js/handlers.js
@@ -7,3 +7,44 @@ export const handleBurgerClick = () => {
     nav.classList.toggle('active');
   });
 };
+
+export const handleMenuItemClick = () => {
+  const burgerButton = document.querySelector('.burger');
+  const nav = document.querySelector('.header__menu');
+  const menuItems = document.querySelectorAll('.header__menu-link');
+
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+      burgerButton.classList.remove('active');
+      nav.classList.remove('active');
+    });
+  });
+};
+
+export const closeOpenNavAuto = () => {
+  const burgerButton = document.querySelector('.burger');
+  const nav = document.querySelector('.header__menu');
+
+  window.addEventListener('resize', () => {
+    const clientWidth = document.body.clientWidth;
+
+    if (clientWidth > 767) {
+      burgerButton.classList.remove('active');
+      nav.classList.remove('active');
+    }
+  });
+};
+
+export const scrollSection = (event) => {
+  const targetValue = event.currentTarget.dataset.target;
+  const $section = document.querySelector(`[data-name="${targetValue}"]`);
+  const $header = document.querySelector('[data-name="header"]');
+
+  const clientPosition = window.pageYOffset;
+  const sectionPosition = $section.getBoundingClientRect().top;
+  const headerHeight = $header.getBoundingClientRect().height;
+  const targetPosition = clientPosition + sectionPosition - headerHeight;
+
+  handleMenuItemClick();
+  window.scrollTo(0, targetPosition);
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-import { handleBurgerClick } from "./handlers.js";
+import {
+  handleBurgerClick,
+  scrollSection,
+  closeOpenNavAuto,
+  handleMenuItemClick,
+} from './handlers.js';
+
+const menuLinks = document.querySelectorAll('[data-btn="menuLink"]');
+const heroButton = document.querySelector('.hero__button');
+
+menuLinks.forEach((item) => item.addEventListener('click', scrollSection));
+heroButton.addEventListener('click', scrollSection);
+
 
 handleBurgerClick();
+handleMenuItemClick();
+closeOpenNavAuto();
